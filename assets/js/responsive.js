57c02963b1f7288a2270d515c7f5be7152908b5f
Muhamad Zulfan <73164719+neunicorn@users.noreply.github.com>
2023-02-25 11:11:19 +0700

    selesai

--- assets/js/responsive.js
+++ assets/js/responsive.js
@@ -1,6 +1,12 @@
 const menuToggle = document.querySelector(".menu-toggle input");
 const nav = document.querySelector("nav ul");
+const checkbox = document.getElementById("checkbox");
 
 menuToggle.addEventListener("click", () => {
   nav.classList.toggle("slide");
+});
+
+nav.addEventListener("click", () => {
+  checkbox.checked = false;
+  nav.classList.remove("slide");
 });
